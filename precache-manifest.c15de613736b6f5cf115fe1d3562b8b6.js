8c874195525bd5d522ab600ed3ad40f0e1c2f20f
Dmitriy Bondarenko <demianbondarenko@gmail.com>
2020-08-09 19:06:50 +0200

    Updates

---
+++ precache-manifest.c15de613736b6f5cf115fe1d3562b8b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95a90cefd7566f56f1fea6ec34ee2bb6",
+    "url": "/tt-hooks/index.html"
+  },
+  {
+    "revision": "80144bcd8292ec829d0a",
+    "url": "/tt-hooks/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7a9b6929d06ee7876576",
+    "url": "/tt-hooks/static/js/2.3509af6b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tt-hooks/static/js/2.3509af6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80144bcd8292ec829d0a",
+    "url": "/tt-hooks/static/js/main.ba4b5cb7.chunk.js"
+  },
+  {
+    "revision": "37c081ea810628d0f50c",
+    "url": "/tt-hooks/static/js/runtime-main.487dbef9.js"
+  }
+]);
